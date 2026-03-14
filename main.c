cfdb45ca96b452ff10589ef62c84ccd0b9fc0854
oestrangeiro <mateus12092004@gmail.com>
2026-03-14 00:28:25 -0300

    Sistema de paginação implementado!

--- main.c
+++ main.c
@@ -8,36 +8,74 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "src/colors.h"
 
 #define PROGRAM_NAME 	"mHex"
 #define SIZE_PER_OFFSET 16
+#define PAGINATOR_DEFAULT_VALUE SIZE_PER_OFFSET
 
 int counterLines = 0;
 
 
 int main (int argc, char *argv[]){
 
-	char fileName[64];
+	char *fileName;
 	unsigned char buffer[16];
 	int bytesRead;
 	unsigned long offset = 0;
+	int opt;
+	
+	// Caso o usuário não informe o valor da paginação, assumo que é 16
+	int paginator = PAGINATOR_DEFAULT_VALUE;
+	
+	/*
+		p: recebe um argumento do tipo inteiro, maior que zero.
+		Sendo esse número a quantidade de linhas de código hexadecimal
+		por página a serem lidos por vez
+	*/
+
 
 	if(argc == 1){
-		printf("Uso ./%s file\n", PROGRAM_NAME);
+		printf("Uso ./%s -f seu_arquivo\n", PROGRAM_NAME);
+		printf("[OPCIONAL] Para paginação, utilize -p seguido de um número\n");
 		return 1;
 	}
 
-	strcpy(fileName, argv[1]);
+	while( (opt = getopt(argc, argv, "f:p:") ) != -1 ){
+		switch(opt){		
+			case 'f':
+				fileName = optarg;
+			break;
+
+			case 'p':
+			
+				paginator = atoi(optarg);
+				
+				if(paginator <= 0) {
+					fprintf(stderr, "[!] ERRO: Valor de paginação deve ser maior que zero!\n");
+					return 1;
+				}
+				
+			break;
+
+			case '?':
+				fprintf(stderr, "[!] Erro: Argumentos mal-informados!\n");			
+				return 1;
+			break;
+		}
+	}	
 
 	FILE *filePtr = fopen(fileName, "r");
 
 	if(filePtr == NULL){
-		printf("[!] Erro ao ler arquivo '%s'!\n", fileName);
-		exit(-1);
+		fprintf(stderr, "[!] ERRO: Erro ao ler arquivo '%s'!\n%s (CODE: %d)\n", fileName, strerror(errno), errno);
+		return 1;
 	}
-	printf(COLOR_RED "[ %s ]\n" COLOR_RESET, fileName);
+	printf(COLOR_RED "[ %s ]\t\t" COLOR_RESET, fileName);
+	printf(COLOR_LIGHT_GREEN "[ Paginação: %d linhas ]\n" COLOR_RESET, paginator);
 	printf("OFFSET:     ");
 	
 	// Coluna de 00 ~ 0F (apenas pra facilitar a visualização)
@@ -77,9 +115,9 @@ int main (int argc, char *argv[]){
 
 		// Essa instrução faz uma paginação a cada 16 linhas
 		// Caso alguém queira usar, só descomentar
-		// if(counterLines % 16 == 0){
-		// 	getchar();
-		// }
+		if(counterLines % paginator == 0){
+			getchar();
+		}
 	}
 
 	return 0;
--- src/colors.h
+++ src/colors.h
@@ -1,10 +1,11 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-#define COLOR_GREEN 	"\033[1;32m"
-#define COLOR_BLUE 		"\033[34m"
-#define COLOR_CYAN 		"\033[36m"
-#define COLOR_RED		"\033[31m"
-#define COLOR_RESET 	"\033[0m"
+#define COLOR_GREEN 		"\033[1;32m"
+#define COLOR_LIGHT_GREEN 	"\033[0;32m"
+#define COLOR_BLUE 			"\033[34m"
+#define COLOR_CYAN 			"\033[36m"
+#define COLOR_RED			"\033[31m"
+#define COLOR_RESET 		"\033[0m"
 
 #endif
